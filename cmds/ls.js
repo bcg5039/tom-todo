e212890472e421de2464a9e943fdc4a65670d230
George Worthington <GeoWorthington@gmail.com>
2017-06-09 12:35:41 -0400

    add command added

--- cmds/ls.js
+++ cmds/ls.js
@@ -1,1 +1,23 @@
 const {get, set} = require('../lib/store.js')
+const { map} = require('ramda')
+
+module.exports = function(){
+  const toDoList = get()
+  console.log(`\n        ToDos`)
+  console.log('_______________')
+  map(printObj, toDoList.todos)
+  console.log('_______________')
+    return
+  }
+
+
+
+function printObj(v){
+  console.log(`${v.id} | [${isComplete(v) ? "X" : " "}] | ${v.text} `)
+  return v
+}
+
+function isComplete(obj)  {
+  return
+obj.completed
+}
