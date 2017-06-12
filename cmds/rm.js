d32f4d56c10b7953b76034a88b1dc21f7cd592e2
George Worthington <GeoWorthington@gmail.com>
2017-06-12 09:14:49 -0400

    partial filter

--- cmds/rm.js
+++ cmds/rm.js
@@ -1,2 +1,27 @@
 const {get, set} = require('../lib/store.js')
-const ls = require('./ls')
+//const ls = require('./ls')
+const { filter, mapAccum, map, propEq } = require ('ramda')
+
+module.exports = function(id){
+const toDoList = get()
+console.log(toDoList)
+console.log(filterRemove(3,toDoList.todos))
+//set(toDoList, filter(filterRemove,toDoList))
+//set(mapAccum(reducer, 1, filter(x => !isId(id,x),list))[1])
+  //console.log(id)
+  //console.log(toDoList.todos)
+return ls()
+}
+
+function filterRemove (id, list) {
+  return list.filter(function(obj) {
+    console.log(id != obj.id)
+    return (id != obj.id)
+  })
+}
+
+//const toDelete = new Set(['abc', 'efg']);
+//const newArray = arrayOfObjects.filter(obj => !toDelete.has(obj.id));
+function findPos (id, list) {
+  return console.log(list.id !== id)
+}
